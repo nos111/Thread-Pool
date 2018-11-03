4d74202348b8ede92a3a532d92981a5120875a95
Nour <nsaffour@gmail.com>
2018-11-03 14:19:42 +0100

    added Pthread create and loop

--- pool.c
+++ pool.c
@@ -8,13 +8,18 @@
 //add thread to queue
 //extract thread for every job
 //return the thread after it finishes execution
+
 void initializeThreads(pthread_t * tids, int threadCount);
+void thread(void * arg);
+void Pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine) (void *), void *arg);
 
 int main(int argc, char ** argv) {
+
         if(argc != 2) {
                 printf("Usage: <thread count> \n");
                 return 0;
         }
+
         pthread_t * tids = malloc(sizeof(pthread_t) * atoi(argv[1])); 
         initializeThreads(tids, atoi(argv[1]));
 
@@ -23,5 +28,21 @@ int main(int argc, char ** argv) {
 }
 
 void initializeThreads(pthread_t * tids, int threadCount) {
+        int * ptr;
+        for(int i = 0; i < threadCount; i++) {
+                ptr = malloc(sizeof(int));
+                *ptr = i;
+                Pthread_create(tids[*ptr], NULL, thread, ptr);
+        }
+}
+
+void Pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine) (void *), void *arg) {
+        if(pthread_create(thread, attr, start_routine, arg) != 0) {
+                perror("error creating thread ");
+        }
+}
+
+void thread(void * arg) {
 
 }
+
